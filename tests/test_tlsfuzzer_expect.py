4545aea4090342010f9b09c9d59d91e4590e6cfc
Hubert Kario <hkario@redhat.com>
2019-04-03 20:10:00 +0200

    support for ecdsa signatures in server CV in TLS 1.3

--- tests/test_tlsfuzzer_expect.py
+++ tests/test_tlsfuzzer_expect.py
@@ -153,6 +153,29 @@ srv_raw_pss_certificate = str(
     )
 
 
+srv_raw_ecdsa_key = str(
+    "-----BEGIN PRIVATE KEY-----\n"
+    "MIGHAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBG0wawIBAQQgCOZr0Ovs0eCmh+XM\n"
+    "QWDYVpsQ+sJdjiq/itp/kYnWNSahRANCAATINGMQAl7cXlPrYzJluGOgmc8sYvae\n"
+    "tO2EsXKYG6lnYhudZiepVYORP8vqLyxCF/bMIuuVKOPWSfsRGo/H8pnK\n"
+    "-----END PRIVATE KEY-----\n"
+    )
+
+
+srv_raw_ecdsa_certificate = str(
+    "-----BEGIN CERTIFICATE-----\n"
+    "MIIBbTCCARSgAwIBAgIJAPM58cskyK+yMAkGByqGSM49BAEwFDESMBAGA1UEAwwJ\n"
+    "bG9jYWxob3N0MB4XDTE3MTAyMzExNDI0MVoXDTE3MTEyMjExNDI0MVowFDESMBAG\n"
+    "A1UEAwwJbG9jYWxob3N0MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEyDRjEAJe\n"
+    "3F5T62MyZbhjoJnPLGL2nrTthLFymBupZ2IbnWYnqVWDkT/L6i8sQhf2zCLrlSjj\n"
+    "1kn7ERqPx/KZyqNQME4wHQYDVR0OBBYEFPfFTUg9o3t6ehLsschSnC8Te8oaMB8G\n"
+    "A1UdIwQYMBaAFPfFTUg9o3t6ehLsschSnC8Te8oaMAwGA1UdEwQFMAMBAf8wCQYH\n"
+    "KoZIzj0EAQNIADBFAiA6p0YM5ZzfW+klHPRU2r13/IfKgeRfDR3dtBngmPvxUgIh\n"
+    "APTeSDeJvYWVBLzyrKTeSerNDKKHU2Rt7sufipv76+7s\n"
+    "-----END CERTIFICATE-----\n"
+    )
+
+
 class TestExpect(unittest.TestCase):
     def test___init__(self):
         exp = Expect(ContentType.handshake)
@@ -2044,6 +2067,40 @@ class TestExpectCertificateVerify(unittest.TestCase):
                                       "sha256",
                                       32)
         scheme = SignatureScheme.rsa_pss_pss_sha256
+        cer_verify = CertificateVerify((3, 4)).create(sig, scheme)
+
+        exp.process(state, cer_verify)
+
+    def test_process_with_ecdsa_sig_alg(self):
+        exp = ExpectCertificateVerify()
+
+        state = ConnectionState()
+        state.cipher = CipherSuite.TLS_AES_128_GCM_SHA256
+        state.version = (3, 4)
+
+        cert = Certificate(CertificateType.x509, (3, 4)).create(
+            X509CertChain([X509().parse(srv_raw_ecdsa_certificate)]))
+
+        private_key = parsePEMKey(srv_raw_ecdsa_key, private=True)
+
+        client_hello = ClientHello()
+        ext = SignatureAlgorithmsExtension().\
+            create([SignatureScheme.ecdsa_secp256r1_sha256])
+        client_hello.extensions = [ext]
+        state.handshake_messages.append(client_hello)
+
+        state.handshake_messages.append(cert)
+
+        hh_digest = state.handshake_hashes.digest('sha256')
+        self.assertEqual(state.prf_name, "sha256")
+        signature_context = bytearray(b'\x20' * 64 +
+                                      b'TLS 1.3, server CertificateVerify' +
+                                      b'\x00') + hh_digest
+        sig = private_key.hashAndSign(signature_context,
+                                      "ecdsa",
+                                      "sha256",
+                                      32)
+        scheme = SignatureScheme.ecdsa_secp256r1_sha256
         cer_verify = CertificateVerify((3, 4)).create(sig, scheme)
 
         exp.process(state, cer_verify)
